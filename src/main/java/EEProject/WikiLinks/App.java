41d06c3deb1e38b2baa7da88281b55c17fa55308
PhillipVolkov <phillipvolkov@gmail.com>
2021-07-21 11:38:46 -0700

    Inital 2

---
+++ src/main/java/EEProject/WikiLinks/App.java
@@ -0,0 +1,13 @@
+package EEProject.WikiLinks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
