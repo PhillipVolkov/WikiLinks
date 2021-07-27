13d73ce367e139a58920e9669b09511aa3426f5a
PhillipVolkov <phillipvolkov@gmail.com>
2021-07-27 14:02:45 -0700

    replaced tokenizer with .split

--- src/main/java/EEProject/WikiLinks/NaiveAlgorithm.java
+++ src/main/java/EEProject/WikiLinks/NaiveAlgorithm.java
@@ -6,7 +6,6 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
 import org.tartarus.snowball.ext.PorterStemmer;
@@ -34,7 +33,7 @@ public class NaiveAlgorithm {
 			"#", "’s", "'s", "<", ">", "--", "=", "\"", "", "#"};
 	
 	static final boolean testLinks = true;
-	static final boolean debugPrint = false;
+	static final boolean debugPrint = true;
 	static final double threshold = 15;
 	static final int maxTokenSeperation = 20;
 	static final int maxTokenQuantity = 3;
@@ -42,7 +41,7 @@ public class NaiveAlgorithm {
 	static final int occurenceDifferencePenalty = 75;
 	static final double titleFactorWeight = 0.4;
 	
-	static final String stemmerDelims = " |\n|:|, |. |-";
+	static final String stemmerDelims = " |\n|:|, |\\. |\\.\n|-";
 	
 	static Hashtable<String, Page> savedPages = new Hashtable<String, Page>();
 	
@@ -81,38 +80,39 @@ public class NaiveAlgorithm {
     	}
     	else {
 	    	ArrayList<String[]> testLinks = new ArrayList<String[]>();
-	    	testLinks.add(new String[] {"Auto DevOps", "https://docs.gitlab.com/ee/user/project/clusters/index.html#auto-devops", "Use Auto DevOps to automate the CI/CD process"});
-	    	testLinks.add(new String[] {"the Prometheus cluster integration is enabled", "https://docs.gitlab.com/ee/user/clusters/integrations.html#prometheus-cluster-integration", 
-	    			"When the Prometheus cluster integration is enabled, GitLab monitors the cluster’s health"});
-	    	testLinks.add(new String[] {"Cluster management project", "https://docs.gitlab.com/ee/user/clusters/management_project.html", 
-	    			"Attach a Cluster management project to your cluster to manage shared resources requiring cluster-admin privileges for installation, such as an Ingress controller"});
-	    	testLinks.add(new String[] {"CI/CD Pipelines", "https://docs.gitlab.com/ee/ci/pipelines/index.html", "Create CI/CD Pipelines to build, test, and deploy to your cluster"});
-	    	testLinks.add(new String[] {"cluster integrations", "https://docs.gitlab.com/ee/user/clusters/integrations.html", "Connect GitLab to in-cluster applications using cluster integrations"});
-	    	testLinks.add(new String[] {"project access tokens", "https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html", "project access tokens"});
-	    	testLinks.add(new String[] {"GitLab to manage your cluster for you", "https://docs.gitlab.com/ee/user/project/clusters/gitlab_managed_clusters.html", "See how to allow GitLab to manage your cluster for you"});
-	    	testLinks.add(new String[] {"Infrastructure as Code", "https://docs.gitlab.com/ee/user/infrastructure", 
-	    			"GitLab provides you with great solutions to help you manage your infrastrucure: Infrastructure as Code and GitOps"});
-	    	testLinks.add(new String[] {"Deploy Boards", "https://docs.gitlab.com/ee/user/project/deploy_boards.html", "Use Deploy Boards to see the health and status of each CI environment running on your Kubernetes cluster"});
-	    	testLinks.add(new String[] {"role-based or attribute-based access controls", "https://docs.gitlab.com/ee/user/project/clusters/cluster_access.html", "Use role-based or attribute-based access controls"});
-	    	testLinks.add(new String[] {"Read more about Kubernetes monitoring", "https://docs.gitlab.com/ee/user/project/integrations/prometheus_library/kubernetes.html", "Read more about Kubernetes monitoring"});
-	    	testLinks.add(new String[] {"Kubernetes with Knative", "https://docs.gitlab.com/ee/user/project/clusters/serverless/index.html", "Run serverless workloads on Kubernetes with Knative"});
-	    	testLinks.add(new String[] {"related documentation", "https://docs.gitlab.com/ee/user/analytics/value_stream_analytics.html#permissions", "Find the current permissions on the Value Stream Analytics dashboard, as described in related documentation"});
-	    	testLinks.add(new String[] {"Through the API", "https://docs.gitlab.com/ee/api/users.html", "You can also create users through the API as an admin"});
+//	    	testLinks.add(new String[] {"Auto DevOps", "https://docs.gitlab.com/ee/user/project/clusters/index.html#auto-devops", "Use Auto DevOps to automate the CI/CD process"});
+//	    	testLinks.add(new String[] {"the Prometheus cluster integration is enabled", "https://docs.gitlab.com/ee/user/clusters/integrations.html#prometheus-cluster-integration", 
+//	    			"When the Prometheus cluster integration is enabled, GitLab monitors the cluster’s health"});
+//	    	testLinks.add(new String[] {"Cluster management project", "https://docs.gitlab.com/ee/user/clusters/management_project.html", 
+//	    			"Attach a Cluster management project to your cluster to manage shared resources requiring cluster-admin privileges for installation, such as an Ingress controller"});
+//	    	testLinks.add(new String[] {"CI/CD Pipelines", "https://docs.gitlab.com/ee/ci/pipelines/index.html", "Create CI/CD Pipelines to build, test, and deploy to your cluster"});
+//	    	testLinks.add(new String[] {"cluster integrations", "https://docs.gitlab.com/ee/user/clusters/integrations.html", "Connect GitLab to in-cluster applications using cluster integrations"});
+//	    	testLinks.add(new String[] {"project access tokens", "https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html", "project access tokens"});
+//	    	testLinks.add(new String[] {"GitLab to manage your cluster for you", "https://docs.gitlab.com/ee/user/project/clusters/gitlab_managed_clusters.html", "See how to allow GitLab to manage your cluster for you"});
+//	    	testLinks.add(new String[] {"Infrastructure as Code", "https://docs.gitlab.com/ee/user/infrastructure", 
+//	    			"GitLab provides you with great solutions to help you manage your infrastrucure: Infrastructure as Code and GitOps"});
+//	    	testLinks.add(new String[] {"Deploy Boards", "https://docs.gitlab.com/ee/user/project/deploy_boards.html", "Use Deploy Boards to see the health and status of each CI environment running on your Kubernetes cluster"});
+//	    	testLinks.add(new String[] {"role-based or attribute-based access controls", "https://docs.gitlab.com/ee/user/project/clusters/cluster_access.html", "Use role-based or attribute-based access controls"});
+//	    	testLinks.add(new String[] {"Read more about Kubernetes monitoring", "https://docs.gitlab.com/ee/user/project/integrations/prometheus_library/kubernetes.html", "Read more about Kubernetes monitoring"});
+//	    	testLinks.add(new String[] {"Kubernetes with Knative", "https://docs.gitlab.com/ee/user/project/clusters/serverless/index.html", "Run serverless workloads on Kubernetes with Knative"});
+//	    	testLinks.add(new String[] {"related documentation", "https://docs.gitlab.com/ee/user/analytics/value_stream_analytics.html#permissions", "Find the current permissions on the Value Stream Analytics dashboard, as described in related documentation"});
+//	    	testLinks.add(new String[] {"Through the API", "https://docs.gitlab.com/ee/api/users.html", "You can also create users through the API as an admin"});
+	    	testLinks.add(new String[] {"Elasticsearch.log file", "https://docs.gitlab.com/ee/administration/logs.html#elasticsearchlog", ""});
 
-	    	testLinks.add(new String[] {"Kubernetes Engine", "https://docs.gitlab.com/ee/user/project/clusters/gitlab_managed_clusters.html", ""});
-	    	testLinks.add(new String[] {"Kubernetes Pipeline", "https://docs.gitlab.com/ee/user/clusters/management_project.html", ""});
-	    	testLinks.add(new String[] {"find users' api tokens", "https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html", ""});
-
-	    	testLinks.add(new String[] {"package", "https://docs.gitlab.com/ee/user/packages/index.html", ""});
-	    	testLinks.add(new String[] {"environment", "https://docs.gitlab.com/ee/ci/environments/index.html", "Use Deploy Boards to see the health and status of each CI environment running on your Kubernetes cluster"});
-	    	testLinks.add(new String[] {"NGINX Ingress", "https://docs.gitlab.com/ee/user/project/integrations/prometheus_library/nginx.html", "Automatic monitoring of NGINX Ingress is also supported"});
-	    	testLinks.add(new String[] {"Kubernetes podlogs", "https://docs.gitlab.com/ee/user/project/clusters/kubernetes_pod_logs.html", "View your Kubernetes podlogs directly in GitLab"});
-	    	testLinks.add(new String[] {"instance", "https://docs.gitlab.com/ee/user/instance/clusters/index.html", "On the group level, to use the same cluster across multiple projects within your group"});
-	    	testLinks.add(new String[] {"group", "https://docs.gitlab.com/ee/user/group/clusters/index.html", "On the instance level, to use the same cluster across multiple groups and projects."});
-	    	
-	    	testLinks.add(new String[] {"developers", "https://docs.gitlab.com/ee/user/permissions.html", "cautionThe whole cluster security is based on a model where developers are trusted, so only trusted users should be allowed to control your clusters"});
-	    	testLinks.add(new String[] {"dynamic names", "https://docs.gitlab.com/ee/ci/environments/index.html", ""});
-	    	testLinks.add(new String[] {"dependency", "https://docs.gitlab.com/ee/user/packages/index.html", ""});
+//	    	testLinks.add(new String[] {"Kubernetes Engine", "https://docs.gitlab.com/ee/user/project/clusters/gitlab_managed_clusters.html", ""});
+//	    	testLinks.add(new String[] {"Kubernetes Pipeline", "https://docs.gitlab.com/ee/user/clusters/management_project.html", ""});
+//	    	testLinks.add(new String[] {"find users' api tokens", "https://docs.gitlab.com/ee/user/project/settings/project_access_tokens.html", ""});
+//
+//	    	testLinks.add(new String[] {"package", "https://docs.gitlab.com/ee/user/packages/index.html", ""});
+//	    	testLinks.add(new String[] {"environment", "https://docs.gitlab.com/ee/ci/environments/index.html", "Use Deploy Boards to see the health and status of each CI environment running on your Kubernetes cluster"});
+//	    	testLinks.add(new String[] {"NGINX Ingress", "https://docs.gitlab.com/ee/user/project/integrations/prometheus_library/nginx.html", "Automatic monitoring of NGINX Ingress is also supported"});
+//	    	testLinks.add(new String[] {"Kubernetes podlogs", "https://docs.gitlab.com/ee/user/project/clusters/kubernetes_pod_logs.html", "View your Kubernetes podlogs directly in GitLab"});
+//	    	testLinks.add(new String[] {"instance", "https://docs.gitlab.com/ee/user/instance/clusters/index.html", "On the group level, to use the same cluster across multiple projects within your group"});
+//	    	testLinks.add(new String[] {"group", "https://docs.gitlab.com/ee/user/group/clusters/index.html", "On the instance level, to use the same cluster across multiple groups and projects."});
+//	    	
+//	    	testLinks.add(new String[] {"developers", "https://docs.gitlab.com/ee/user/permissions.html", "cautionThe whole cluster security is based on a model where developers are trusted, so only trusted users should be allowed to control your clusters"});
+//	    	testLinks.add(new String[] {"dynamic names", "https://docs.gitlab.com/ee/ci/environments/index.html", ""});
+//	    	testLinks.add(new String[] {"dependency", "https://docs.gitlab.com/ee/user/packages/index.html", ""});
 	    	
 	    	double startTime = System.nanoTime();
 	    	for (String[] linkPair : testLinks) {
@@ -139,12 +139,12 @@ public class NaiveAlgorithm {
 	    		final boolean subSection = linkPair[1].indexOf("#") != -1;
 	    		
 	    		//link stemming
-			    StringTokenizer  tokenizer = new StringTokenizer(linkPair[0].toLowerCase(), stemmerDelims);
+			    String[] tokenized = linkPair[0].toLowerCase().split(stemmerDelims);
     		    PorterStemmer stemmer = new PorterStemmer();
     		    
-    		    while (tokenizer.hasMoreElements()) {
-    		    	String token = tokenizer.nextToken().toLowerCase().strip();
-    		    	token = String.join(" ", token.split("\n"));
+    		    for (String token : tokenized) {
+    		    	token = token.toLowerCase().strip();
+    		    	token = String.join("", token.split("\n"));
     		    	
     		    	stemmer.setCurrent(token);
     		    	stemmer.stem();
@@ -184,36 +184,36 @@ public class NaiveAlgorithm {
 	    			}
 
 					ArrayList<String> section;
-	    			if (searchPage.getPermaLinks().containsKey(linkPair[1].split("#")[1])) {
+//	    			if (searchPage.getPermaLinks().containsKey(linkPair[1].split("#")[1])) {
 	    				section = searchPage.getPermaLinkSection(linkPair[1].split("#")[1]);
-	    				stringTitle = "";
-	    				
+//	    				stringTitle = "";
+//	    				
 	    				if (debugPrint) System.out.println(section);
-	    				
-	    				int offset = 0;
-	    				for (int i = 0; i < headingTokens.length; i++) {
-	    					if (!contains(stopWords, headingTokens[i]) && !headingTokens[i].equals("")) {
-	    						stringTitle += String.join(" ", section.get(i-offset).split(" |\\.|-")) + " ";
-	    					}
-	    					else {
-	    						stringTitle += headingTokens[i] + " ";
-	    						offset--;
-	    					}
-	    				}
-	    			}
-	    			else {
+//	    				
+//	    				int offset = 0;
+//	    				for (int i = 0; i < headingTokens.length; i++) {
+//	    					if (!contains(stopWords, headingTokens[i]) && !headingTokens[i].equals("")) {
+//	    						stringTitle += String.join(" ", section.get(i-offset).split(" |\\.|-")) + " ";
+//	    					}
+//	    					else {
+//	    						stringTitle += headingTokens[i] + " ";
+//	    						offset--;
+//	    					}
+//	    				}
+//	    			}
+//	    			else {
 	    				stringTitle = String.join(" ", headingTokens);	   
-	    			}
+//	    			}
 	    		}
 	    		
 	    		//title stems
 	    		ArrayList<String> title = new ArrayList<String>();
-	    		tokenizer = new StringTokenizer(stringTitle.toLowerCase(), stemmerDelims);
+	    		tokenized = stringTitle.toLowerCase().split(stemmerDelims);
     		    stemmer = new PorterStemmer();
     		    
-    		    while (tokenizer.hasMoreElements()) {
-    		    	String token = tokenizer.nextToken().toLowerCase().strip();
-    		    	token = String.join(" ", token.split("\n"));
+    		    for (String token : tokenized) {
+    		    	token = token.toLowerCase().strip();
+    		    	token = String.join("", token.split("\n"));
     		    	
     		    	stemmer.setCurrent(token);
     		    	stemmer.stem();
@@ -223,7 +223,8 @@ public class NaiveAlgorithm {
     		    		title.add(token);
     		    	}
     		    }
-			    
+    		    
+    		    
 			    if (debugPrint) {
 			    	System.out.println(linkPair[0]);
 			    	System.out.println(tokens);
@@ -768,12 +769,14 @@ public class NaiveAlgorithm {
             for (String section : sections) {
     	    	ArrayList<String> tokens = new ArrayList<String>();
     	    	
-    		    StringTokenizer  tokenizer = new StringTokenizer(section.toLowerCase(), stemmerDelims);
     		    PorterStemmer stemmer = new PorterStemmer();
     		    
-    		    while (tokenizer.hasMoreElements()) {
-    		    	String token = tokenizer.nextToken().toLowerCase().strip();
-    		    	token = String.join(" ", token.split("\n"));
+    		    String[] tokenized = section.toLowerCase().split(stemmerDelims);
+    		    stemmer = new PorterStemmer();
+    		    
+    		    for (String token : tokenized) {
+    		    	token = token.toLowerCase().strip();
+    		    	token = String.join("", token.split("\n"));
     		    	
     		    	stemmer.setCurrent(token);
     		    	stemmer.stem();
@@ -784,6 +787,7 @@ public class NaiveAlgorithm {
     		    	}
     		    }
 
+    		    
     	    	stems.add(tokens);
             }
 
